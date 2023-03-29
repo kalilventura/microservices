0147d35d06f649fce0f6949beae727c840487eab
kalilventura <kalilventur@hotmail.com>
2023-03-28 23:51:48 -0300

    creating dockerfile and docker compose

--- sales-api/app.js
+++ sales-api/app.js
@@ -2,11 +2,11 @@ import restify from 'restify';
 
 const server = restify.createServer();
 const env = process.env;
-const PORT = env.PORT || 8080;
+const PORT = env.PORT || 8082;
 
 server.get('/api/status', (req, res, next) => {
     res.send(200, {
-        service: 'Auth-API',
+        service: 'Sales-API',
         status: 'UP'
     });
     next();
