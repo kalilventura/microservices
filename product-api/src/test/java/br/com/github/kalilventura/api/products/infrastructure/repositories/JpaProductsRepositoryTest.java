18ef8ab370151ebc134f35b5499955ecf9fff3b3
kalilventura <kalilventur@hotmail.com>
2023-04-25 23:22:23 -0300

    initializing product repository test

--- product-api/src/test/java/br/com/github/kalilventura/api/products/infrastructure/repositories/JpaProductsRepositoryTest.java
+++ product-api/src/test/java/br/com/github/kalilventura/api/products/infrastructure/repositories/JpaProductsRepositoryTest.java
@@ -7,7 +7,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.parallel.Isolated;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
@@ -15,10 +17,12 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+@Isolated
+@DataJpaTest
+//@SpringBootTest
 @Tag("integration")
 @ActiveProfiles("test")
-@ExtendWith(SpringExtension.class)
+//@ExtendWith(SpringExtension.class)
 class JpaProductsRepositoryTest {
 
     private final ProductsRepository repository;
